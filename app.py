9a762db17aaaacef64d4b4fdd4110d3d4a252a80
AbhinavSharma1402 <abhi.sh1402@gmail.com>
2026-04-23 01:57:01 +0530

    to upload multiple files

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import streamlit as st
+import os
 
 st.title("Research Paper Assistant")
 st.write("Upload papers and ask questions")
+
+os.makedirs("uploads", exist_ok=True)
+
 uploaded = st.file_uploader("Upload PDF", type="pdf")
 
 if uploaded:
-    with open("uploads/temp.pdf", "wb") as f:
+    path = os.path.join("uploads", uploaded.name)
+
+    with open(path, "wb") as f:
         f.write(uploaded.read())
+
+    st.success("File saved successfully!")
 
 from pdf_parser import extract_text
 
 if uploaded:
-    text = extract_text("uploads/temp.pdf")
+    text = extract_text(path)
     st.text_area("Extracted Text", text[:3000])
