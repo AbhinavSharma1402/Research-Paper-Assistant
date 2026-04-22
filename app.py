ce7b7da785aedf817089fa0620efe7770367fe0d
AbhinavSharma1402 <abhi.sh1402@gmail.com>
2026-04-23 00:57:31 +0530

    file uploader created

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 st.title("Research Paper Assistant")
 st.write("Upload papers and ask questions")
+uploaded = st.file_uploader("Upload PDF", type="pdf")
+
+if uploaded:
+    with open("uploads/temp.pdf", "wb") as f:
+        f.write(uploaded.read())
