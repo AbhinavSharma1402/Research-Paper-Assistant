f2495a2a0dade2533c413e6c29bb73723c886040
AbhinavSharma1402 <abhi.sh1402@gmail.com>
2026-04-24 01:23:32 +0530

    To upload multiple files

--- app.py
+++ app.py
@@ -6,18 +6,25 @@ st.write("Upload papers and ask questions")
 
 os.makedirs("uploads", exist_ok=True)
 
-uploaded = st.file_uploader("Upload PDF", type="pdf")
+uploaded_files = st.file_uploader(
+    "Upload 10–15 Research Papers",
+    type=["pdf"],
+    accept_multiple_files=True
+)
 
-if uploaded:
-    path = os.path.join("uploads", uploaded.name)
-
-    with open(path, "wb") as f:
-        f.write(uploaded.read())
-
-    st.success("File saved successfully!")
+if uploaded_files:
+    for uploaded in uploaded_files:
+        path = os.path.join("uploads", uploaded.name)
+        with open(path, "wb") as f:
+            f.write(uploaded.read())
+    st.success(f"{len(uploaded_files)} files uploaded successfully!")
 
 from pdf_parser import extract_text
 
-if uploaded:
-    text = extract_text(path)
-    st.text_area("Extracted Text", text[:3000])
+if uploaded_files:
+    all_text = ""
+    for uploaded in uploaded_files:
+        path = os.path.join("uploads", uploaded.name)
+        text = extract_text(path)
+        all_text += text + "\n\n"
+    st.text_area("Extracted Text", all_text[:3000])
